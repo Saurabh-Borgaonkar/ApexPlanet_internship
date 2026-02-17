cd2df71587d333f04702181076ef0e46eb0c6ad8
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-02-17 18:28:12 +0530

    task_1

---
+++ Task1/script.js
@@ -0,0 +1,11 @@
+// function click(){
+// addEventListener('click',()=>{
+// alert("its not safe");
+// console.log("alert");
+// })
+// }
+// click();
+
+btn.addEventListener("click", function() {
+    alert("hey,ApexPlanet");
+});
